d7032729f66722ed8db5504df51250f7b2ef9847
wenjing.chen <wenjing.chen@yingzt.com>
2020-02-17 16:15:03 +0800

    70 爬楼梯循环版本

--- 70.爬楼梯.swift
+++ 70.爬楼梯.swift
@@ -10,12 +10,17 @@ class Solution {
         guard n != 1 else {
             return 1
         }
-        
-        guard n != 2 else{
+        guard n != 2 else {
             return 2
         }
         
-        return climbStairs(n-1) + climbStairs(n-2)
+        var results = Array<Int>.init(repeating: 1, count: n+1)
+        results[1] = 1
+        results[2] = 2
+        for i in 2...n {
+            results[i] = results[i-1] + results[i-2]
+        }
+        return results[n]
     }
 }
 // @lc code=end
