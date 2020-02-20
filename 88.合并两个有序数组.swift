1011bbcfc767e209ee7f703eb1f5c7712ac708ab
wenjing.chen <wenjing.chen@yingzt.com>
2020-02-20 11:23:46 +0800

    88.合并两个有序数组, 从右到左扫描元素，不需要移动元素

--- 88.合并两个有序数组.swift
+++ 88.合并两个有序数组.swift
@@ -7,29 +7,28 @@
 // @lc code=start
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var firstIndex = 0, secondIndex = 0
-        while firstIndex < m + secondIndex && secondIndex < n {
-            if nums1[firstIndex] > nums2[secondIndex] {
-                moveOneStepRight(nums: &nums1, from: firstIndex, to: m+secondIndex)
-                nums1[firstIndex] = nums2[secondIndex]
-                secondIndex += 1
+        var firstIndex = m - 1, secondIndex = n - 1
+        var index = m + n - 1
+        while firstIndex >= 0 && secondIndex >= 0 {
+            if nums1[firstIndex] >= nums2[secondIndex] {
+                nums1[index] = nums1[firstIndex]
+                firstIndex -= 1
+            }else{
+                nums1[index] = nums2[secondIndex]
+                secondIndex -= 1
             }
-            firstIndex += 1
+            index -= 1
         }
         
-        while secondIndex < n {
-            nums1[firstIndex] = nums2[secondIndex]
-            firstIndex += 1
-            secondIndex += 1
-        }
-    }
-    
-    func moveOneStepRight(nums : inout [Int], from: Int, to: Int){
-        for i in ((from+1)...to).reversed(){
-            nums[i] = nums[i - 1]
+        while secondIndex >= 0 {
+            nums1[index] = nums2[secondIndex]
+            secondIndex -= 1
+            index -= 1
+            
         }
     }
 }
+
 
 // @lc code=end
 
