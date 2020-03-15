c45582550fc2baa80e41089de03c9b806633c0c3
wenjing <84336951@qq.com>
2020-03-15 15:08:15 +0800

    [127] 单词接龙 使用队列，性能仍然无法通过，应该是哪里错了

--- 127.单词接龙.swift
+++ 127.单词接龙.swift
@@ -5,6 +5,57 @@
  */
 
 // @lc code=start
+let NotFound = -1
+
+public struct Queue<T> {
+  fileprivate var array = [T?]()
+  fileprivate var head = 0
+
+  public var isEmpty: Bool {
+    return count == 0
+  }
+
+  public var count: Int {
+    return array.count - head
+  }
+
+  public mutating func enqueue(_ element: T) {
+    array.append(element)
+  }
+
+  public mutating func dequeue() -> T? {
+    guard let element = array[guarded: head] else { return nil }
+
+    array[head] = nil
+    head += 1
+
+    let percentage = Double(head)/Double(array.count)
+    if array.count > 50 && percentage > 0.25 {
+      array.removeFirst(head)
+      head = 0
+    }
+
+    return element
+  }
+
+  public var front: T? {
+    if isEmpty {
+      return nil
+    } else {
+      return array[head]
+    }
+  }
+}
+
+extension Array {
+    subscript(guarded idx: Int) -> Element? {
+        guard (startIndex..<endIndex).contains(idx) else {
+            return nil
+        }
+        return self[idx]
+    }
+}
+
 struct Node{
     let word: String
     let level: Int
@@ -12,18 +63,17 @@ struct Node{
 
 class Solution {
     func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
-        var queue = [Node]()
+        var queue = Queue<Node>()
         var remainWordList = wordList
-        queue.append(Node.init(word: beginWord, level: 1))
-        while let node = queue.first {
-            queue.remove(at: 0)
+        queue.enqueue(Node.init(word: beginWord, level: 1))
+        while let node = queue.dequeue() {
             if node.word == endWord {
                 return node.level
             }
             
             let newBeginWords = extractOneCharDifferentWords(beginWorkd: node.word, wordList: remainWordList)
             for newBegin in newBeginWords {
-                queue.append(Node.init(word: newBegin, level: node.level + 1))
+                queue.enqueue(Node.init(word: newBegin, level: node.level + 1))
             }
             
             remainWordList.removeAll { string -> Bool in
@@ -60,7 +110,7 @@ class Solution {
             }
         }
         
-        return differrentCount <= 1
+        return differrentCount == 1
     }
 }
 // @lc code=end
