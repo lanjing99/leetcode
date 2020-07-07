71e886b0b5688f04db11c707d9f115c98eb9a03d
wenjing <84336951@qq.com>
2020-07-07 14:59:12 +0800

    完成11.盛最多水的容器

--- 11.盛最多水的容器.swift
+++ 11.盛最多水的容器.swift
@@ -6,16 +6,33 @@
 
 // @lc code=start
 class Solution {
-    func maxArea(_ heights: [Int]) -> Int {
-        var areas = [Int]()
-        for i in 0..<heights.count {
-            for j in (i+1)..<heights.count {
-                let h = heights[i] < heights[j] ? heights[i] : heights[j]
-                let width = j - i
-                areas.append(h*width)
+    func maxArea(_ height: [Int]) -> Int {
+        var leftIndex = 0, rightIndex = height.count - 1
+        
+        var result = 0
+        while rightIndex > leftIndex {
+            let area = areaOfHeights(height, leftIndex: leftIndex, rightIndex: rightIndex)
+            if area > result {
+                result = area
+            }
+            
+            //判断下一个可能的最大区域的一条边是左边还是右边
+            if height[rightIndex] > height[leftIndex] {
+                leftIndex += 1
+            }else{
+                rightIndex -= 1
             }
         }
-        return areas.max()!
+        return result
+    }
+    
+    //根据左边和右边的索引计算面积
+    func areaOfHeights(_ heights: [Int], leftIndex: Int, rightIndex: Int) -> Int{
+        var height = heights[leftIndex]
+        if heights[rightIndex] < height {
+            height = heights[rightIndex]
+        }
+        return (rightIndex - leftIndex) * height
     }
 }
 // @lc code=end
