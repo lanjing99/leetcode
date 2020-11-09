5f1b3dfb85f455d287bbadc44c7c54b83adff512
wenjing <84336951@qq.com>
2020-11-09 10:26:03 +0800

    重新写了一遍三数之和，去除重复的没搞清楚

--- 15.三数之和.py
+++ 15.三数之和.py
@@ -10,39 +10,43 @@ import math
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
+        result :List[List[int]] = []
         nums.sort()
-
-        result: List[List[int]] = []
-        numsLen = len(nums)
-        for i in range(0, numsLen - 2):
-            if i != 0 and nums[i] == nums[i-1]:
+        target = 0
+        listLength = len(nums)
+        while target < listLength - 2:
+            left = target + 1
+            right = listLength - 1
+            # 去除重复的i
+            if target > 0 and nums[target] == nums[target-1]:
+                target += 1
                 continue
-
-            left = i + 1
-            right = numsLen - 1
+            
             while left < right:
-                # 去除左边的重复
-                if left != i+1 and nums[left] == nums[left -1]:
+                #
+                if left != target + 1 and nums[left] == nums[left - 1]:
                     left += 1
                     continue
-                # 去除右边的重复
-                if right != numsLen - 1 and nums[right] == nums[right + 1]:
+                
+                if right != listLength - 1 and nums[right] == nums[right + 1]:
                     right -= 1
-                    continue 
-
-                if -nums[i] == nums[left] + nums[right]:
-                    result.append([nums[i], nums[left], nums[right]])
+                    continue
+                
+                if nums[left] + nums[right] + nums[target] < 0:
                     left += 1
+
+                elif nums[left] + nums[right] + nums[target] > 0:
                     right -= 1
-
-                elif -nums[i] > nums[left] + nums[right]:
-                    left += 1
+                    
                 else:
+                    result.append([nums[target], nums[left], nums[right]])
+                    left += 1
                     right -= 1
-
+       
+            target += 1   
         return result
 
+print (Solution().threeSum([-1,0,1,2,-1,-4]))
 
 # @lc code=end
 
