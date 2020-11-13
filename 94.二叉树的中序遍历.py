2ff305c167f20c0e083b2e4598c304576e256944
wenjing <84336951@qq.com>
2020-11-13 10:29:11 +0800

    94.二叉树的中序遍历 不破坏原来树的状态

--- 94.二叉树的中序遍历.py
+++ 94.二叉树的中序遍历.py
@@ -7,13 +7,13 @@
 # @lc code=start
 # Definition for a binary tree node.
 
-from typing import List
+# from typing import List
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
         
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
@@ -22,21 +22,24 @@ class Solution:
             return result
         
         stack = [root]
-        while len(stack) != 0:
-            while stack[-1].left is not None:
-                stack.append(stack[-1].left)
-                stack[-2].left = None
-                
-            top = stack.pop()
-            result.append(top.val)
-            if top.right is not None:
-                stack.append(top.right)
-        return result
+        current = root
+       
+        while current is not None or len(stack) != 0:
+            while current is not None and current.left is not None:
+                stack.append(current.left)
+                current = current.left
     
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
+            if current is not None:
+                stack.append(current)
 
-three = TreeNode(3)
-two = TreeNode(2, three, None)    
-one = TreeNode(1, None, two)
-print(Solution().inorderTraversal(one))
+        return result
+        
+# three = TreeNode(3)
+# two = TreeNode(2, three, None)    
+# one = TreeNode(1, None, two)
+# print(Solution().inorderTraversal(one))
 # @lc code=end
 
