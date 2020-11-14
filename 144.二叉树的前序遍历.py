a7dcc59ef4f3fa23de2e1d5741f46a4445424b44
wenjing <84336951@qq.com>
2020-11-14 11:27:39 +0800

    144.二叉树的前序遍历.py 是用栈来迭代

--- 144.二叉树的前序遍历.py
+++ 144.二叉树的前序遍历.py
@@ -6,24 +6,43 @@
 
 # @lc code=start
 # Definition for a binary tree node.
+# from typing import List
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         result = []
-        self.helper(root, result)
+        if root is None:
+            return result
+        
+        # result.append(root.val)
+        stack = []
+        current = root
+        
+        while len(stack) > 0 or current is not None:
+            # 遍历左子树        
+            while current is not None:
+                        result.append(current.val)
+                stack.append(current)
+                current = current.left
+                
+            current = stack.pop()
+            
+            if current.right is not None:
+                current = current.right
+            else:
+                current = None
+    
         return result
     
-    def helper(self, node: TreeNode, result: List[int]):
-        if node is None:
-            return
-        
-        result.append(node.val)
-        self.helper(node.left, result)
-        self.helper(node.right, result)
-       
+# three = TreeNode(3)
+# two = TreeNode(2, three, None)    
+# one = TreeNode(1, None, two)
+
+# print(Solution().preorderTraversal(one))  
 # @lc code=end
 
